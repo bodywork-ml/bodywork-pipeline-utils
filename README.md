f3e15cf4c0ceab4444801fb5c7b597c254925906
alexioannides <alex.ioanndies@yahoo.co.uk>
2021-05-21 21:36:21 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+<div align="center">
+<img src="https://bodywork-media.s3.eu-west-2.amazonaws.com/website_logo_transparent_background_full.png"/>
+</div>
+---
+
+# Bodywork Pipeline Utilities
+
+TODO
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages, setup
+
+
+# get package version
+with open('VERSION') as version_file:
+    version = version_file.read().strip()
+
+# assemble requirements
+with open('requirements_pkg.txt') as f:
+    requirements_pkg = f.read().splitlines()
+
+with open('requirements_dev.txt') as f:
+    requirements_dev = f.read().splitlines()
+
+# load the README file and use it as the long_description for PyPI
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='bodywork_pipeline_utils',
+    description='TODO',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    version=version,
+    license='Apache 2.0',
+    author='Bodywork Machine Learning Ltd.',
+    author_email='info@bodyworkml.com',
+    url='https://www.bodyworkml.com',
+    project_urls={
+        'Source': 'https://github.com/bodywork-ml/bodywork-pipeline-utils'
+    },
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    python_requires=">=3.8.*",
+    install_requires=requirements_pkg,
+    extras_require={
+        'dev': requirements_dev
+    },
+    zip_safe=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
+)
