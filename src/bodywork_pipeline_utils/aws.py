ebbc4879d93e4c9ebde6465080ef6a686c7b0975
alexioannides <alex.ioanndies@yahoo.co.uk>
2021-07-09 11:13:13 +0100

    Document AWS code.

--- src/bodywork_pipeline_utils/aws.py
+++ src/bodywork_pipeline_utils/aws.py
@@ -2,21 +2,140 @@
 Utility functions for working with AWS services.
 """
 from datetime import datetime
-from typing import Optional
+from typing import NamedTuple, Optional
 from re import findall, sub
 
 import boto3 as aws
-from pandas import DataFrame
+from botocore.exceptions import ClientError
+from pandas import DataFrame, read_csv, read_parquet
 
 
-def get_latest_dataset_from_s3(bucket: str, folder: str) -> DataFrame:
-    pass
+FILE_FORMAT_EXTENSIONS = {"csv": "CSV", "parquet": "PARQUET"}
+
+s3_client = aws.client("s3")
 
 
-def _extract_iso_timestamp_from_string(s: str) -> Optional[datetime]:
-    ts_string = findall(r"\d{4}\W\d{2}\W\d{2}[\sT]\d{2}\W\d{2}\W\d{2}", s)
-    if ts_string:
-        ts_std = sub(r"[\sT](\d{2})\W(\d{2})\W(\d{2})", r"T\1:\2:\3", ts_string[0])
-        return datetime.fromisoformat(ts_std)
-    else:
-        return None
+class Dataset(NamedTuple):
+    """Container for downloaded datasets."""
+
+    data: DataFrame
+    datetime: datetime
+    key: str
+
+
+class S3DatasetObject:
+    """Model for remote tabular datasets on S3."""
+
+    def __init__(self, bucket: str, s3_obj_key: str):
+        """Constructor.
+
+        Args:
+            bucket: S3 bucket name.
+            s3_obj_key: Key of object in bucket.
+
+        Raises:
+            ValueError: If timestamp and a supported file format cannot
+                be parsed from the object key.
+        """
+        self._bucket = bucket
+        self._s3_obj_key = s3_obj_key
+        _datetime = self._extract_iso_timestamp_from_string(s3_obj_key)
+        if _datetime is None:
+            msg = f"{s3_obj_key} has no parsable timestamp."
+            raise ValueError(msg)
+        _file_format = self._extract_file_format_from_string(s3_obj_key)
+        if _file_format is None:
+            msg = f"{s3_obj_key} has no supported file format."
+            raise ValueError(msg)
+        self._datetime = _datetime
+        self._file_format = _file_format
+
+    def get(self) -> DataFrame:
+        """Get dataset from S3.
+
+        Raises:
+            RuntimeError: If the dataset cannot be retrieved from S3.
+
+        Returns:
+            DataFrame representation of remote dataset.
+        """
+        try:
+            s3_object = s3_client.get_object(Bucket=self._bucket, Key=self._s3_obj_key)
+            data_stream = s3_object["Body"]
+        except ClientError as e:
+            msg = f"cannot get data from s3://{self._bucket}/{self._s3_obj_key} - {e}"
+            raise RuntimeError(msg)
+        if self._file_format == "CSV":
+            dataset = read_csv(data_stream)
+        elif self._file_format == "PARQUET":
+            dataset = read_parquet(data_stream)
+        return dataset
+
+    @property
+    def timestamp(self) -> datetime:
+        return self._datetime
+
+    @property
+    def file_format(self) -> str:
+        return self._file_format
+
+    @property
+    def obj_key(self) -> str:
+        return self._s3_obj_key
+
+    def __lt__(self, other: "S3DatasetObject") -> bool:
+        return self._datetime < other._datetime
+
+    @staticmethod
+    def _extract_iso_timestamp_from_string(s: str) -> Optional[datetime]:
+        regex = r"\d{4}\W\d{2}\W\d{2}[\sT]\d{2}\W\d{2}\W\d{2}|\d{4}\W\d{2}\W\d{2}"
+        ts_string = findall(regex, s)
+        if ts_string:
+            ts_std = sub(r"[\sT](\d{2})\W(\d{2})\W(\d{2})", r"T\1:\2:\3", ts_string[0])
+            return datetime.fromisoformat(ts_std)
+        else:
+            return None
+
+    @staticmethod
+    def _extract_file_format_from_string(s: str) -> Optional[str]:
+        file_format_extension = findall(r"\.(\w+)$", s)
+        if file_format_extension:
+            return FILE_FORMAT_EXTENSIONS.get(file_format_extension[0].lower())
+        else:
+            return None
+
+
+def get_latest_dataset_from_s3(bucket: str, folder: str = "") -> Dataset:
+    """Get the most recent timestamped dataset from an S3 folder.
+
+    Args:
+        bucket: S3 bucket to look in.
+        folder: Folder within bucket to limit search, defaults to "".
+
+    Raises:
+        RuntimeError: If no files are found or if there was an error
+            connecting to AWS.
+
+    Returns:
+        A Dataset object containing the data and metadata about the file.
+    """
+    folder_std = folder if folder.endswith("/") or folder == "" else f"{folder}/"
+    try:
+        s3_objects = s3_client.list_objects(Bucket=bucket, Prefix=folder_std)
+        s3_datasets = []
+        for s3_obj in s3_objects["Contents"]:
+            try:
+                dataset_obj = S3DatasetObject(bucket, s3_obj["Key"])
+                s3_datasets.append(dataset_obj)
+            except ValueError:
+                pass
+
+        latest_file = sorted(s3_datasets)[-1]
+        datset = Dataset(latest_file.get(), latest_file.timestamp, latest_file.obj_key)
+        return datset
+    except IndexError:
+        msg = f"no valid dataset files found in s3://{bucket}/{folder_std}"
+        raise RuntimeError(msg)
+    except Exception as e:
+        msg = f"failed to download dataset from s3://{bucket}/{folder_std}"
+        raise RuntimeError(msg) from e
--- tests/test_aws.py
+++ tests/test_aws.py
@@ -2,10 +2,14 @@
 Tests for AWS utilities.
 """
 from datetime import datetime
-from bodywork_pipeline_utils.aws import (
-    _extract_iso_timestamp_from_string,
-    get_latest_dataset_from_s3
-)
+from unittest.mock import MagicMock, patch
+
+from botocore.exceptions import ClientError
+from pandas.core.frame import DataFrame
+
+from pytest import raises
+
+from bodywork_pipeline_utils.aws import get_latest_dataset_from_s3, S3DatasetObject
 
 
 def test_extract_iso_timestamp_from_string_identifies_iso_timestamps():
@@ -14,7 +18,110 @@ def test_extract_iso_timestamp_from_string_identifies_iso_timestamps():
     filename3 = "some_dataset_2021-07-07 13|33|13.csv"
     filename4 = "there_is_no_date_here.csv"
     expected_dt = datetime(2021, 7, 7, 13, 33, 13)
-    assert _extract_iso_timestamp_from_string(filename1) == expected_dt
-    assert _extract_iso_timestamp_from_string(filename2) == expected_dt
-    assert _extract_iso_timestamp_from_string(filename3) == expected_dt
-    assert _extract_iso_timestamp_from_string(filename4) is None
+    assert S3DatasetObject._extract_iso_timestamp_from_string(filename1) == expected_dt
+    assert S3DatasetObject._extract_iso_timestamp_from_string(filename2) == expected_dt
+    assert S3DatasetObject._extract_iso_timestamp_from_string(filename3) == expected_dt
+    assert S3DatasetObject._extract_iso_timestamp_from_string(filename4) is None
+
+
+def test_extract_iso_timestamp_from_string_identifies_iso_dates():
+    filename1 = "2021-07-07.csv"
+    filename2 = "some_dataset_2021-07-07.csv"
+    expected_dt = datetime(2021, 7, 7)
+    assert S3DatasetObject._extract_iso_timestamp_from_string(filename1) == expected_dt
+    assert S3DatasetObject._extract_iso_timestamp_from_string(filename2) == expected_dt
+
+
+def test_extract_file_format_from_string_matches_file_formats():
+    filename1 = "2021-07-07.csv"
+    filename2 = "2021-07-07.parquet"
+    filename3 = "unknown_file_format"
+    filename4 = "unsupported_file.format"
+    assert S3DatasetObject._extract_file_format_from_string(filename1) == "CSV"
+    assert S3DatasetObject._extract_file_format_from_string(filename2) == "PARQUET"
+    assert S3DatasetObject._extract_file_format_from_string(filename3) is None
+    assert S3DatasetObject._extract_file_format_from_string(filename4) is None
+
+
+def test_s3datasetobject_raises_exception_for_invalid_dataset_keys():
+    with raises(ValueError, match="has no parsable timestamp"):
+        S3DatasetObject("my-bucket", "this_is_not_a_dataset_key")
+
+    with raises(ValueError, match="has no supported file format"):
+        S3DatasetObject("my-bucket", "my/data_2021-12-12T13:30:30")
+
+    with raises(ValueError, match="has no supported file format"):
+        S3DatasetObject("my-bucket", "my/data_2021-12-12T13:30:30.foo")
+
+
+def test_s3datasetobject_construction_with_valid_data():
+    assert S3DatasetObject("my-bucket", "my_data_2021-12-12T13:30:30.csv") is not None
+
+
+def test_s3datasetobject_less_than_operator():
+    obj1 = S3DatasetObject("my-bucket", "my_data_2021-11-12T13:30:30.csv")
+    obj2 = S3DatasetObject("my-bucket", "my_data_2021-12-12T13:30:30.csv")
+    assert obj1 < obj2
+
+
+@patch("bodywork_pipeline_utils.aws.s3_client")
+def test_s3datasetobject_gets_csv_datasets(mock_s3_client: MagicMock):
+    mock_s3_client.get_object.return_value = {
+        "Body": open("tests/resources/dataset.csv", "rb")
+    }
+    data = S3DatasetObject("my-bucket", "my/data_2020-01-01T00:00:00.csv").get()
+    assert type(data) == DataFrame
+    assert data.shape == (2, 2)
+
+
+@patch("bodywork_pipeline_utils.aws.s3_client")
+def test_s3datasetobject_gets_parquet_datasets(mock_s3_client: MagicMock):
+    mock_s3_client.get_object.return_value = {
+        "Body": open("tests/resources/dataset.parquet", "rb")
+    }
+    data = S3DatasetObject("my-bucket", "my/data_2020-01-01T00:00:00.parquet").get()
+    assert type(data) == DataFrame
+    assert data.shape == (2, 2)
+
+
+@patch("bodywork_pipeline_utils.aws.s3_client")
+def test_s3datasetobject_raises_exception_on_s3_client_error(mock_s3_client: MagicMock):
+    mock_s3_client.get_object.side_effect = ClientError({}, "")
+    with raises(RuntimeError, match="cannot get data from s3"):
+        S3DatasetObject("my-bucket", "my/data_2020-01-01T00:00:00.csv").get()
+
+
+@patch("bodywork_pipeline_utils.aws.s3_client")
+def test_get_latest_dataset_from_s3_gets_latest_dataset(mock_s3_client: MagicMock):
+    mock_s3_client.list_objects.return_value = {
+        "Contents": [
+            {"Key": "datasets/my_data_2020-06-08T00:00:00.csv"},
+            {"Key": "datasets/my_data_2020-07-08T01:00:00.csv"},
+            {"Key": "datasets/my_data_2020-05-08T00:15:00.csv"},
+        ]
+    }
+    mock_s3_client.get_object.return_value = {
+        "Body": open("tests/resources/dataset.csv", "rb")
+    }
+    dataset = get_latest_dataset_from_s3("my-bucket", "datasets")
+    assert type(dataset.data) == DataFrame
+    assert dataset.datetime == datetime(2020, 7, 8, 1)
+    assert dataset.key == "datasets/my_data_2020-07-08T01:00:00.csv"
+
+
+@patch("bodywork_pipeline_utils.aws.s3_client")
+def test_get_latest_dataset_from_s3_raises_exception_when_no_datasets_found(
+    mock_s3_client: MagicMock,
+):
+    mock_s3_client.list_objects.return_value = {"Contents": []}
+    with raises(RuntimeError, match="no valid dataset files found in s3"):
+        get_latest_dataset_from_s3("my-bucket", "datasets")
+
+
+@patch("bodywork_pipeline_utils.aws.s3_client")
+def test_get_latest_dataset_from_s3_raises_exception_when_s3_client_fails(
+    mock_s3_client: MagicMock,
+):
+    mock_s3_client.list_objects.side_effect = Exception
+    with raises(RuntimeError, match="failed to download dataset"):
+        get_latest_dataset_from_s3("my-bucket", "datasets")
